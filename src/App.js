420fae13452a6ea0cbef812ce5bf89590841a547
unknown <matthewpiedra1228@gmail.com>
2020-07-10 21:13:47 -0400

    Added update and updateFinished methods, and modified TaskList component

--- src/App.js
+++ src/App.js
@@ -1,66 +1,108 @@
-import React, {Component} from 'react';
-import TaskList from './TaskList';
-import ToDoForms from './ToDoForms';
+import React, { Component } from "react";
+import TaskList from "./TaskList";
+import ToDoForms from "./ToDoForms";
 
 class ToDo extends Component {
-   constructor() {
-     super();
-     
-     this.state = {
-       todos: [],
-       currentToDo: "",
-     }
-   }
-   
-   handleChange = event => {
-      this.setState({
-        currentToDo: event.target.value,
-      });
-   }
+  constructor() {
+    super();
 
-   addItem = event => {
-     event.preventDefault(); 
-     
-     if(document.getElementsByTagName("input")[0].value !== "") {
-        let {todos, currentToDo} = this.state;
-     
-        let list = todos.slice();
-        list.push(currentToDo);
-        
-        this.setState({
-          todos: list,
-        });
+    this.state = {
+      todos: [],
+      currentToDo: "",
+      isUpdating: false,
+      indexBeingUpdated: null,
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      currentToDo: event.target.value,
+    });
+  };
+
+  addItem = (event) => {
+    event.preventDefault();
+
+    if (document.getElementsByTagName("input")[0].value !== "") {
+      let { todos, currentToDo } = this.state;
+
+      let list = todos.slice();
+      list.push(currentToDo);
+
+      // assign list to todo and reset text field after submitting form
+      this.setState({
+        todos: list,
+        currentToDo: "",
+      });
+    }
+  };
+
+  clear = () => {
+    this.setState({
+      todos: [],
+      currentToDo: "",
+    });
+  };
+
+  delete = (index) => {
+    const filteredItems = this.state.todos.filter((el, idx) => {
+      return idx !== index;
+    });
+
+    this.setState({
+      todos: filteredItems,
+    });
+  };
+
+  update = (index) => {
+    this.setState({
+      isUpdating: true,
+      indexBeingUpdated: index,
+    });
+  };
+
+  updateFinished = index => {
+    const { todos } = this.state;
+
+    const inputField = document.getElementsByTagName("li")[index].firstElementChild;
+    
+    let list = todos.map((item, idx) => {
+      if(idx === index) {
+        if(inputField.value !== "") {
+          let updatedItem = inputField.value;
+          return updatedItem;
+        }
       }
-      
-     // reset text field after submitting form
-     document.getElementsByTagName("input")[0].value = "";
-   }
-   
-   clear = () => {
-     this.setState({
-       todos: [],
-       currentToDo: "",
-     });
-   }
+      return item;
+    });
 
-   delete = index => {
-      const filteredItems = this.state.todos.filter((el, idx) => {
-        return idx !== index;
-      });
-      
-      this.setState({
-         todos: filteredItems,
-      });
-   }
+    this.setState({
+      todos: list,
+      isUpdating: false,
+      indexBeingUpdated: null,
+    });
+  }
 
-   render() {
-      return (
-        <div className='container'>
-          <ToDoForms addItem={this.addItem} clear={this.clear} currentToDo={this.state.currentToDo} handleChange={this.handleChange}/>
-          <TaskList todos={this.state.todos} deleteItems = {this.delete}/>
-        </div>
-      );
-   }
+  render() {
+    return (
+      <div className="container">
+        <ToDoForms
+          addItem={this.addItem}
+          clear={this.clear}
+          currentToDo={this.state.currentToDo}
+          handleChange={this.handleChange}
+        />
+        <TaskList
+          todos={this.state.todos}
+          isUpdating={this.state.isUpdating}
+          indexBeingUpdated={this.state.indexBeingUpdated}
+          updateFinished={this.updateFinished}
+          deleteItems={this.delete}
+          updateItems={this.update}
+        />
+      </div>
+    );
+  }
 }
 
 export default ToDo;
--- src/TaskList.js
+++ src/TaskList.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const TaskList = props => {
-  let {todos, deleteItems} = props;
+  const {todos, isUpdating, updateFinished, indexBeingUpdated, deleteItems, updateItems} = props;
 
   return (
     <ul> 
-    {todos.map((todo,index) => 
-     <li key={index}> {todo} <button type="delete" onClick={() => deleteItems(index)}> delete </button> </li>
-    )}
+    {todos.map((todo,index) => { 
+       if(isUpdating && indexBeingUpdated === index) {
+         return <li key={index}> <input type="text" defaultValue={todo}/> <button type="update" onClick={() => { updateFinished(index) }}> update </button> </li>
+       }
+       return <li key={index}> {todo} <button type="delete" onClick={() => deleteItems(index)}> delete </button> <button type="update" onClick={() => updateItems(index)}> update </button> </li>
+      })}
    </ul>
   );
 }
